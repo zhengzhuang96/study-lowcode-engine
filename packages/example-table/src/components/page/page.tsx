6d9962c1156b8d6c199ad06a7ffc6397f2ce95d1
zhengzhuang <zhengzhuang@yuanduyun.com>
2024-03-21 22:46:37 +0800

    feat: 测试univerjs表格

--- packages/example-table/src/components/page/page.tsx
+++ packages/example-table/src/components/page/page.tsx
@@ -2,20 +2,31 @@
  * @Author: {zhengzhuang}
  * @Date: 2024-03-21 21:21:37
  * @LastEditors: {zhengzhuang}
- * @LastEditTime: 2024-03-21 21:56:12
- * @Description:
+ * @LastEditTime: 2024-03-21 22:45:27
+ * @Description: 
  */
 import * as React from 'react';
 import { createElement } from 'react';
-import { HotTable } from '@handsontable/react';
+import "@univerjs/design/lib/index.css";
+import "@univerjs/ui/lib/index.css";
+import "@univerjs/sheets-ui/lib/index.css";
+import "@univerjs/sheets-formula/lib/index.css";
+import "@univerjs/sheets-numfmt/lib/index.css";
 
-import 'handsontable/dist/handsontable.full.css';
-import "handsontable/languages/zh-CN";
+import { LocaleType, LogLevel, Univer } from "@univerjs/core";
+import { defaultTheme } from "@univerjs/design";
+import { UniverDocsPlugin } from "@univerjs/docs";
+import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
+import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
+import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
+import { UniverSheetsPlugin } from "@univerjs/sheets";
+import { UniverSheetsFormulaPlugin } from "@univerjs/sheets-formula";
+import { UniverSheetsNumfmtPlugin } from "@univerjs/sheets-numfmt";
+import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
+import { UniverUIPlugin } from "@univerjs/ui";
+import { DEFAULT_WORKBOOK_DATA_DEMO } from "./constants";
+
 import './index.scss';
-
-// 注册所有模块
-import { registerAllModules } from 'handsontable/registry';
-registerAllModules();
 
 export interface PageProps {
   color?: string;
@@ -27,22 +38,39 @@ const Page: React.FC<PageProps> = function Page({
   style = {},
   ...otherProps
 }) {
+  // univer
+  const univer = new Univer({
+    theme: defaultTheme,
+    locale: LocaleType.ZH_CN,
+    logLevel: LogLevel.VERBOSE,
+  });
+
+  // core plugins
+  univer.registerPlugin(UniverDocsPlugin, {
+    hasScroll: false,
+  });
+  univer.registerPlugin(UniverDocsUIPlugin);
+  univer.registerPlugin(UniverRenderEnginePlugin);
+  univer.registerPlugin(UniverUIPlugin, {
+    container: "index",
+    header: true,
+    toolbar: true,
+    footer: true,
+  });
+  univer.registerPlugin(UniverSheetsPlugin);
+  univer.registerPlugin(UniverSheetsUIPlugin);
+
+  // sheet feature plugins
+
+  univer.registerPlugin(UniverSheetsNumfmtPlugin);
+  univer.registerPlugin(UniverFormulaEnginePlugin);
+  univer.registerPlugin(UniverSheetsFormulaPlugin);
+
+  // create univer sheet instance
+  univer.createUniverSheet(DEFAULT_WORKBOOK_DATA_DEMO);
 
   return (
-    <HotTable
-      data={[
-        ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
-        ['2019', 10, 11, 12, 13],
-        ['2020', 20, 11, 14, 13],
-        ['2021', 30, 15, 12, 13]
-      ]}
-      rowHeaders={true}
-      colHeaders={true}
-      height="auto"
-      autoWrapRow={true}
-      autoWrapCol={true}
-      licenseKey="non-commercial-and-evaluation" // for non-commercial use only
-    />
+    <div id="index" style={{ height: '100vh' }} />
   );
 };
 
--- packages/example-table/src/index.tsx
+++ packages/example-table/src/index.tsx
@@ -2,7 +2,7 @@
  * @Author: {zhengzhuang}
  * @Date: 2024-03-21 21:15:35
  * @LastEditors: {zhengzhuang}
- * @LastEditTime: 2024-03-21 21:57:01
+ * @LastEditTime: 2024-03-21 22:31:38
  * @Description:
  */
 export type { ColorfulButtonProps } from './components/colorful-button';
@@ -11,8 +11,8 @@ export { default as ColorfulButton } from './components/colorful-button';
 export type { ColorfulInputProps } from './components/colorful-input';
 export { default as ColorfulInput } from './components/colorful-input';
 
-// export type { PageProps } from './components/page';
-// export { default as Page } from './components/page';
+export type { PageProps } from './components/page';
+export { default as Page } from './components/page';
 
 const bizCssPrefix = 'bizpack';
 
